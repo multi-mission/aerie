25fe726be8332bc2553c1e25836c583daea2a954
Alexander M Greer <Alexander.M.Greer@jpl.nasa.gov>
2024-09-05 16:25:36 -0700

    first commit for command expansion

--- examples/command-model/src/main/java/gov/nasa/jpl/aerie/command_model/sequencing/command_dictionary/CompilingDictionaryImpl.java
+++ examples/command-model/src/main/java/gov/nasa/jpl/aerie/command_model/sequencing/command_dictionary/CompilingDictionaryImpl.java
@@ -1,40 +1,28 @@
 package gov.nasa.jpl.aerie.command_model.sequencing.command_dictionary;
 
 import gov.nasa.jpl.aerie.command_model.activities.commands.*;
+import gov.nasa.jpl.aerie.command_model.generated.ActivityTypes;
 import gov.nasa.jpl.aerie.command_model.sequencing.Command;
+import gov.nasa.jpl.aerie.merlin.protocol.types.InstantiationException;
+import gov.nasa.jpl.aerie.merlin.protocol.types.SerializedValue;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class CompilingDictionaryImpl implements CompilingDictionary {
 
     @Override
     public CommandActivity interpret(Command command) {
-        // A real definition of this might want to use reflection + a read-in command dictionary file
-        // to automagically match up the command arguments and named CommandActivity parameters.
-        // We're going to fake that part for now.
-        switch (command.stem()) {
-            case "CMD_NO_OP" -> {
-                return new CMD_NO_OP();
-            }
-            case "CMD_WAIT" -> {
-                var activity = new CMD_WAIT();
-                activity.seconds = Double.valueOf(command.arguments().get(0));
-                return activity;
-            }
-            case "CMD_ECHO" -> {
-                var activity = new CMD_ECHO();
-                activity.message = command.arguments().get(0);
-                return activity;
-            }
-            case "CMD_POWER_ON" -> {
-                var activity = new CMD_POWER_ON();
-                activity.device = command.arguments().get(0);
-                return activity;
-            }
-            case "CMD_POWER_OFF" -> {
-                var activity = new CMD_POWER_OFF();
-                activity.device = command.arguments().get(0);
-                return activity;
-            }
-            default -> throw new RuntimeException("Stem %s is not a recognized command.".formatted(command.stem()));
-        }
+      var valueMapper = ActivityTypes.directiveTypes.get(command.stem());
+      var outputType = valueMapper.getInputAsOutput().getSchema();
+      var argumentMap = IntStream.range(0, valueMapper.getInputType().getParameters().size()).boxed().collect(
+          Collectors.toMap(i -> valueMapper.getInputType().getParameters().get(i).name(),
+                           i -> SerializedValue.of(command.arguments().get(i)))); // might be able to use the parameter value schema?
+      try{
+        return (CommandActivity) valueMapper.getInputType().instantiate((Map<String, SerializedValue>) argumentMap);
+      } catch(Exception e) {
+        throw new RuntimeException("Stem %s is not a recognized command.".formatted(command.stem()));
+      }
     }
 }
